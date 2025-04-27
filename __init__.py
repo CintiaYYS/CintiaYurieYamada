32cfea5da8baa7c7134b81a785af18d9bc0fb8d5
CintiaYYS <cintiayys@gmail.com>
2025-04-26 23:03:15 -0300

    Executando

--- __init__.py
+++ __init__.py
@@ -1,8 +1,13 @@
-from codigo_fonte.processamento.csv_utilidades import postsComURL
-from codigo_fonte.processamento.imagens_utilidades import comparar_todas_imagens_recorte
+from codigo_fonte.processamento.csv_utilidades import  ETL_posts, classifica_encontrado_desaparecido
+from codigo_fonte.processamento.imagens_utilidades import  comparar_todas_imagens_recorte, executar
 from codigo_fonte.scrapping.facebook_scraper_playwright import baixarImagens
 
 if __name__ == "__main__":
-    postsComURL()
-    baixarImagens()
-    #comparar_todas_imagens_recorte()
+
+        # print("Iniciando sistema")
+        # ETL_posts()
+        # baixarImagens()
+        # comparar_todas_imagens_recorte()
+        # classifica_encontrado_desaparecido()
+        print("Iniciando busca")
+        executar()
